38f528a4f8c08e1a63387e198436ba7ff520835b
obazeec <obazee.esosa@yahoo.com>
2025-03-14 20:01:08 +0000

    element not well placed in large view, in the hearder section

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -9,3 +9,28 @@ document.getElementById('getYear').textContent = year;
 
 let modify = document.lastModified;
 document.getElementById('lastModified').textContent = modify;
+
+// Hamburger menu
+const mainnav = document.querySelector('.navbody');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+mainnav.classList.toggle('show');
+hambutton.classList.toggle('show');
+});
+
+// toggle the page black or white
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("blac")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "blac";
+	}
+});
